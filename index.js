b2572992176e38576c438ac7c64e8e590317f924
Asmairfan202023 <160836108+Asmairfan202023@users.noreply.github.com>
2024-06-09 12:35:34 +0500

    bank_App|

---
+++ index.js
@@ -0,0 +1,112 @@
+// index.ts
+import { faker } from '@faker-js/faker';
+import inquirer from 'inquirer';
+// BankAccount Class
+class BankAccount {
+    balance;
+    constructor(initialBalance = 0) {
+        this.balance = initialBalance;
+    }
+    debit(amount) {
+        if (this.balance < amount) {
+            return `Transaction cancelled. Insufficient balance. Current balance: $${this.balance}`;
+        }
+        this.balance -= amount;
+        return `Debited $${amount}. New balance: $${this.balance}`;
+    }
+    credit(amount) {
+        this.balance += amount;
+        if (amount > 100) {
+            this.balance -= 1; // Deduct $1 if more than $100 is credited
+            return `Credited $${amount}. $1 fee deducted. New balance: $${this.balance}`;
+        }
+        return `Credited $${amount}. New balance: $${this.balance}`;
+    }
+    checkBalance() {
+        return this.balance;
+    }
+}
+// Customer Class
+class Customer {
+    firstName;
+    lastName;
+    age;
+    mobileNumber;
+    bankAccount;
+    constructor(firstName, lastName, age, mobileNumber, bankAccount) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+        this.bankAccount = bankAccount;
+    }
+}
+// Generate customers with fake data
+const generateCustomers = (numCustomers) => {
+    const customers = [];
+    for (let i = 0; i < numCustomers; i++) {
+        const firstName = faker.person.firstName();
+        const lastName = faker.person.lastName();
+        const age = faker.number.int({ min: 18, max: 70 });
+        let mobileNumber = parseInt(faker.string.numeric('3##-########'));
+        const bankAccount = new BankAccount(faker.number.int({ min: 100, max: 1000 }));
+        customers.push(new Customer(firstName, lastName, age, mobileNumber, bankAccount));
+    }
+    return customers;
+};
+// Main function
+const main = async () => {
+    const customers = generateCustomers(10);
+    const customerChoices = customers.map((customer, index) => ({
+        name: `${customer.firstName} ${customer.lastName}`,
+        value: index
+    }));
+    const { selectedCustomerIndex } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'selectedCustomerIndex',
+            message: 'Select a customer:',
+            choices: customerChoices
+        }
+    ]);
+    const customer = customers[selectedCustomerIndex];
+    console.log(`Welcome ${customer.firstName} ${customer.lastName} to the Bank App!`);
+    const promptLoop = async () => {
+        const answers = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: ['Check Balance', 'Credit Money', 'Debit Money', 'Exit']
+            },
+            {
+                type: 'input',
+                name: 'amount',
+                message: 'Enter the amount:',
+                when: (answers) => answers.action !== 'Check Balance' && answers.action !== 'Exit',
+                validate: (value) => {
+                    const valid = !isNaN(parseFloat(value));
+                    return valid || 'Please enter a number';
+                },
+                filter: Number
+            }
+        ]);
+        switch (answers.action) {
+            case 'Check Balance':
+                console.log(`Your current balance is: $${customer.bankAccount.checkBalance()}`);
+                break;
+            case 'Credit Money':
+                console.log(customer.bankAccount.credit(answers.amount));
+                break;
+            case 'Debit Money':
+                console.log(customer.bankAccount.debit(answers.amount));
+                break;
+            case 'Exit':
+                console.log('Thank you for using the Bank App!');
+                return;
+        }
+        await promptLoop();
+    };
+    await promptLoop();
+};
+main();
